1a31580262181db7f637b14cca6f8e61afd01326
Shumbusho43 <davidshumbusho10@gmail.com>
2023-06-18 18:52:35 +0200

    modified testing

--- testing.js
+++ testing.js
@@ -25,40 +25,7 @@ const doc = {
       name: "api_key",
       in: "header"
     },
-    petstore_auth: {
-      type: "oauth2",
-      authorizationUrl: "https://petstore.swagger.io/oauth/authorize",
-      flow: "implicit",
-      scopes: {
-        read_pets: "read your pets",
-        write_pets: "modify pets in your account"
-      }
-    }
   },
-  definitions: {
-    User: {
-      name: "Jhon Doe",
-      age: 29,
-      parents: {
-        father: "Simon Doe",
-        mother: "Marie Doe"
-      },
-      diplomas: [{
-        school: "XYZ University",
-        year: 2020,
-        completed: true,
-        internship: {
-          hours: 290,
-          location: "XYZ Company"
-        }
-      }]
-    },
-    AddUser: {
-      $name: "Jhon Doe",
-      $age: 29,
-      about: ""
-    }
-  }
 }
 
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
